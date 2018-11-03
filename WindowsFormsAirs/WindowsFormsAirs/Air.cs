2df3c8d99afb7d33951a280363f6dd16b86861cc
Dyrno4kin <dyrno4kin@gmail.com>
2018-11-03 20:43:11 +0400

    Lab_4

--- WindowsFormsAirs/WindowsFormsAirs/Air.cs
+++ WindowsFormsAirs/WindowsFormsAirs/Air.cs
@@ -23,7 +23,6 @@ namespace WindowsFormsAirs
         /// <param name="maxSpeed">Максимальная скорость</param>
         /// <param name="weight">Вес самолета</param>
         /// <param name="mainColor">Основной цвет кузова</param>
-
         public Air(int maxSpeed, float weight, Color mainColor)
         {
             MaxSpeed = maxSpeed;
